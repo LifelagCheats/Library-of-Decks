1ab7f159974a24f8970392f3a57c2ca0ef7c7c3a
LifelagCheats <179240443+LifelagCheats@users.noreply.github.com>
2026-06-28 21:56:59 -0400

    Update parser to comply with newly added card's initial conditionals.

--- frontend/src/scripts/ts/cardprocessor.ts
+++ frontend/src/scripts/ts/cardprocessor.ts
@@ -7,6 +7,8 @@ export function processDie(trigger: string): string {
   parsed = parsed.replace(/On hit/g,
     '<span class="trigger-onhit"> On Hit </span>')
 
+  parsed = parsed.replace(/On clash win/g, '<span class="trigger-clashwin">On Clash Win</span>')
+
   parsed = parsed.replace(/none/g, '')
   return parsed;
 }

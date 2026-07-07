5533f603845e616bdca891ed19186ef055533540
LifelagCheats <179240443+LifelagCheats@users.noreply.github.com>
2026-07-07 16:45:56 -0400

    feat(dev): Added stylized effect descriptions and effect icons beside them. more effects will be added to the database as the tool grows.

--- frontend/src/scripts/ts/cardprocessor.ts
+++ frontend/src/scripts/ts/cardprocessor.ts
@@ -1,14 +1,98 @@
-export function processDie(trigger: string): string {
-  let parsed: string = trigger;
+const triggers = {
+  "On use": {
+    className: "trigger-onuse",
+    display: "On Use"
+  },
 
-  parsed = parsed.replace(/On use/g,
-    '<span class="trigger-onuse"> On Use </span>');
+  "On hit": {
+    className: "trigger-onhit",
+    display: "On Hit"
+  },
 
-  parsed = parsed.replace(/On hit/g,
-    '<span class="trigger-onhit"> On Hit </span>');
+  "On clash win": {
+    className: "trigger-clashwin",
+    display: "On Clash Win"
+  },
 
-  parsed = parsed.replace(/On clash win/g, '<span class="trigger-clashwin">On Clash Win</span>');
+  "none": {
+    className: "",
+    display: ""
+  }
+};
 
-  parsed = parsed.replace(/none/g, '');
+const keywords = {
+  Burn: {
+    className: "status-burn",
+    icon: "/icons/status/burn.png"
+  },
+
+  Bleed: {
+    className: "status-bleed",
+    icon: "/icons/status/bleed.png"
+  },
+
+  Fragile: {
+    className: "status-fragile",
+    icon: "/icons/status/fragile.png"
+  },
+
+  Protection: {
+    className: "status-protection",
+    icon: "/icons/status/protection.png"
+  },
+
+  Haste: {
+    className: "status-haste",
+    icon: "/icons/status/haste.png"
+  },
+
+  Charge: {
+    className: "status-charge",
+    icon: "/icons/status/charge.png"
+  },
+
+  Light: {
+    className: "status-light",
+    icon: "/icons/status/light.png"
+  }
+};
+
+export function processTrigger(trigger: string): string {
+  const keyword = triggers[trigger as keyof typeof triggers];
+
+  if (!keyword) {
+    return trigger;
+  }
+
+  if (!keyword.className) {
+    return "";
+  }
+
+  return `<span class="${keyword.className}">${keyword.display}</span>`;
+}
+
+export function processDescription(description: string): string {
+  let parsed = description;
+
+  for (const [word, keyword] of Object.entries(keywords)) {
+    const regex = new RegExp(`\\b${word}\\b`, "g");
+
+    parsed = parsed.replace(
+    regex,
+    `
+    <span class="keyword ${keyword.className}">
+      <span class="icon-wrapper">
+      <img 
+        src="${keyword.icon}" 
+        alt="${word}"
+        class="status-icon"
+      />
+      </span>
+      ${word}
+    </span>
+    `
+    );
+  }
+
   return parsed;
 }
